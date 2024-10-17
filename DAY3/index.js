5a10a89ce0fc252f8a66b13b0dfdc84a5f1d82b4
swetasingh <singhsweta8588@gmail.com>
2024-10-17 13:38:48 +0530

    first commit

---
+++ DAY3/index.js
@@ -0,0 +1,18 @@
+document.write("swift")
+let v = 5
+v = 8
+console.log(v)
+
+for(let i=0;i<5;i++){
+    console.log(i)
+}
+let i=5;
+while(i<10){
+    console.log(i);
+    i++;
+}
+
+const arr=[0,"bca1st year"];
+console.log(arr);
+console.log(typeof(arr[0]));
+
